4e0c2a4ef2acb72a0c42aa8dd8cac5d7470bef5d
arturkot95@gmail.com <arturkot95@gmail.com>
2022-03-17 18:51:43 +0100

    squash with init

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Home from 'components/pages/Home.jsx';
-import Backoffice from 'components/pages/Backoffice';
-import ExerciseSet from 'components/pages/ExerciseSet.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/learn/:titleUrl/*" element={<ExerciseSet />} />
-      <Route path="/backoffice/*" element={<Backoffice />} />
     </Routes>
   </Router>
 );
