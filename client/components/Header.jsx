e4d5308b4c9606014d985deab51996e56b30e086
arturkot95@gmail.com <arturkot95@gmail.com>
2022-03-17 18:52:13 +0100

    header with view toggle

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Toggle } from 'components/ui/Toggle.jsx';
 
-export default function () {
+export default function ({ onViewChange }) {
   return (
-    <header className="my-5">
-      <div className="text-center">
-        <h1 className="text-5xl inline-block">Photo Gallery</h1>
-        <h2 className="text-xl inline-block ml-1">for Workate</h2>
+    <header className="my-5 px-8">
+      <div className="flex items-end justify-center">
+        <div className="flex-1 seld-center">
+          <h1 className="text-5xl inline-block">Photo Gallery</h1>
+          <h2 className="text-xl inline-block ml-1">for Workate</h2>
+        </div>
+        <Toggle label="Carousel View" onChange={onViewChange} />
       </div>
     </header>
   );
