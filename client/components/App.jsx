2d338f927ce6c69a97654e23c3a557fd9bd5a8ad
arturkot95@gmail.com <arturkot95@gmail.com>
2022-03-17 19:00:47 +0100

    pass view to Gallery

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from 'components/Header.jsx';
 import Gallery from 'components/Gallery.jsx';
 
 export default function () {
+  const [view, setView] = useState(false);
+
   return (
     <>
-      <Header />
-      <Gallery />
+      <Header onViewChange={(v) => setView(v)} />
+      <Gallery view={view} />
     </>
   );
 }
