dca1e890067a46ab7f92814b7d847164109bec68
arturkot95@gmail.com <arturkot95@gmail.com>
2022-03-17 19:08:59 +0100

    sticky header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,7 @@ import { Toggle } from 'components/ui/Toggle.jsx';
 
 export default function ({ onViewChange }) {
   return (
-    <header className="my-5 px-8">
+    <header className="py-5 px-8 sticky top-0 z-20 bg-white">
       <div className="flex items-end justify-center">
         <div className="flex-1 seld-center">
           <h1 className="text-5xl inline-block">Photo Gallery</h1>
